ca500766201de0f737065d5bce713bd018451071
Maxime Lorrillere <maxime.lorrillere@lip6.fr>
2015-02-12 18:58:51 +0100

    remotecache: fix deadlock in metadata BUSY flag synchronization

--- metadata.c
+++ metadata.c
@@ -53,7 +53,7 @@ void wait_on_remotecache_page_metadata(void *word, int bit)
 
 void __remotecache_metadata_clear_busy(struct remotecache_inode *inode, pgoff_t index)
 {
-	unsigned long *entry, flags;
+	unsigned long *entry;
 	struct busy_page *bp;
 
 	lockdep_assert_held(&inode->lock);
@@ -62,15 +62,16 @@ void __remotecache_metadata_clear_busy(struct remotecache_inode *inode, pgoff_t 
 			REMOTECACHE_TAG_BUSY);
 	BUG_ON(!entry);
 
+	smp_mb__after_clear_bit();
+
 	rc_debug("%s inode %lu index %lu value %lx\n", __func__, inode->ino,
 			index, *entry);
 
 	rcu_read_lock();
 	hash_for_each_possible_rcu(busy_pages_hash, bp, hash, inode->ino^index) {
 		if (bp->ino == inode->ino && bp->index == index) {
-			pr_err("%s inode %lu index %lu bp %p\n", __func__, inode->ino, index, bp);
+			rc_debug("%s inode %lu index %lu bp %p\n", __func__, inode->ino, index, bp);
 			atomic_set(&bp->state, 0);
-			break;
 		}
 	}
 	rcu_read_unlock();
@@ -105,26 +106,45 @@ void remotecache_metadata_wait_busy(struct remotecache_inode *inode, pgoff_t ind
 		rcu_read_unlock();
 		return;
 	}
+	rcu_read_unlock();
 
 	spin_lock_irqsave(&busy_pages_lock, flags);
 	hash_add_rcu(busy_pages_hash, &bp.hash, bp.ino^bp.index);
 	spin_unlock_irqrestore(&busy_pages_lock, flags);
+
+	/*
+	 * Concurrent wait busy/clear busy: BUSY tag may have been cleared and
+	 * waiters woken up before we add bp to the hash table.
+	 */
+	synchronize_rcu();
+	rcu_read_lock();
+	if (!radix_tree_tag_get(&inode->pages_tree, index,
+			REMOTECACHE_TAG_BUSY)) {
+		rcu_read_unlock();
+		goto out;
+	}
 	rcu_read_unlock();
 
-	pr_err("%s wait on inode %lu index %lu bp %p\n", __func__, inode->ino, index, &bp);
+	rc_debug("%s wait on inode %lu index %lu bp %p\n", __func__, inode->ino, index, &bp);
 
 	while (atomic_read(&bp.state) != 0) {
 		DEFINE_WAIT(wait);
 
 		prepare_to_wait(&wait_busy_page, &wait, TASK_UNINTERRUPTIBLE);
-		if (atomic_read(&bp.state) != 0)
-			schedule();
+		if (atomic_read(&bp.state) != 0 && schedule_timeout(15*HZ)) {
+			WARN_ON(true);
+			finish_wait(&wait_busy_page, &wait);
+			goto out;
+		}
 		finish_wait(&wait_busy_page, &wait);
 	}
 
+out:
 	spin_lock_irqsave(&busy_pages_lock, flags);
 	hash_del_rcu(&bp.hash);
 	spin_unlock_irqrestore(&busy_pages_lock, flags);
 
-	pr_err("%s done on inode %lu index %lu bp %p\n", __func__, inode->ino, index, &bp);
+	synchronize_rcu();
+
+	rc_debug("%s done on inode %lu index %lu bp %p\n", __func__, inode->ino, index, &bp);
 }
