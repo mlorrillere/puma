3afa6e421a8ed6f457949460c5538d4e2d632477
Maxime Lorrillere <maxime.lorrillere@lip6.fr>
2015-07-31 10:02:35 +0200

    remotecache: small fix in heartbeat.h

--- heartbeat.h
+++ heartbeat.h
@@ -1,10 +1,13 @@
 /*
  *	heartbeat.c
  *
- *	Copyright (C) 2012
+ *	Copyright (C) 2015
  *	Maxime Lorrillere <maxime.lorrillere@lip6.fr>
  *	LIP6 - Laboratoire d'Informatique de Paris 6
  */
+
+#ifndef HEARTBEAT_H
+#define HEARTBEAT_H
 
 struct remotecache_node;
 struct remotecache_session;
@@ -14,3 +17,5 @@ void heartbeat_start(struct remotecache_node *node);
 void heartbeat_stop(struct remotecache_node *node);
 void heartbeat_handle_ping(struct remotecache_session *, struct rc_msg *);
 void heartbeat_handle_pong(struct remotecache_session *, struct rc_msg *);
+
+#endif /* HEARTBEAT_H */
